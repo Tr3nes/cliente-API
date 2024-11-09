44d9f6b6bb4710a8c78da3da79689d598a56fc07
Beto Brenes <beto3nes@gmail.com>
2024-11-08 19:55:30 -0600

    Final prueba API en GO

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cliente-API
+
+go 1.23.2
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"time"
+)
+
+type Terremoto struct {
+	Properties struct {
+		Mag   float64 `json:"mag"`
+		Place string  `json:"place"`
+		Time  int64   `json:"time"`
+	} `json:"properties"`
+}
+
+type Respuesta struct {
+	Features []Terremoto `json:"features"`
+}
+
+func main() {
+	// URL para consultar terremotos del día 15 a 20 de noviembre del 2024
+	//url := "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2024-10-15&endtime=2024-10-20"
+	// Misma URL pero ahora se establece para que únicamente sea con una magnitud minima de 5
+	url := "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2024-10-15&endtime=2024-10-20&minmagnitude=5"
+
+	// Hacemos la solicitud HTTP GET
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalf("Error al hacer la solicitud: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Leemos y procesamos la respuesta
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Error al leer la respuesta: %v", err)
+	}
+
+	var datosTerremoto Respuesta
+	err = json.Unmarshal(body, &datosTerremoto)
+	if err != nil {
+		log.Fatalf("Error al decodificar JSON: %v", err)
+	}
+
+	//Impresión de la información de cada terremoto que se saca de la página
+	fmt.Println("Terremotos recientes:")
+	for _, quake := range datosTerremoto.Features {
+		fmt.Printf("Magnitud: %.1f, Lugar: %s, Fecha: %s\n",
+			quake.Properties.Mag,
+			quake.Properties.Place,
+			time.UnixMilli(quake.Properties.Time).Format("2006-01-02 15:04:05"), //Dicha fecha es un Standard del formato de GO
+		)
+	}
+}
